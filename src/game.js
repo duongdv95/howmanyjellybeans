5f476dc04b5131059c708114eaa3689b879f3905
Daniel <danielvanduong@gmail.com>
2019-04-08 22:55:54 -0700

    Heroku test

--- src/game.js
+++ src/game.js
@@ -2,7 +2,7 @@ import React from "react";
 import axios from "axios"
 import io from "socket.io-client";
 import Footer from './footer.js';
-var socket = io("https://jellybean-app.herokuapp.comgit st");
+var socket = io("https://jellybean-app.herokuapp.com");
 // change to "http://localhost:5000" for development
 
 // Components Hierarchy
