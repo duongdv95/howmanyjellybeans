e1fb1a0d86cdddab64aa5fc757bd5136986085c4
Daniel <danielvanduong@gmail.com>
2019-04-25 23:14:43 -0700

    Lightsail proxy test

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,6 +2,7 @@ const proxy = require('http-proxy-middleware');
 const env   = process.env.NODE_ENV || "development"
 
 if(env === "development") {
+    console.log("proxied")
     module.exports = function(app) {
       app.use(proxy('/api', { target: 'http://localhost:5000/'}));
     };
