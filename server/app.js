856948343075c4ff639d15babd2b1a4d1b393191
Daniel <danielvanduong@gmail.com>
2019-04-26 04:47:37 -0700

    SSL test

--- server/app.js
+++ server/app.js
@@ -21,8 +21,11 @@ var hskey, hscert, hschain
 hskey     = fs.readFileSync('/etc/letsencrypt/live/howmanyjellybeans.com-0002/fullchain.pem', "utf-8")
 hscert    = fs.readFileSync('/etc/letsencrypt/live/howmanyjellybeans.com-0002/cert.pem', "utf-8")
 hschain   = fs.readFileSync('/etc/letsencrypt/live/howmanyjellybeans.com-0002/chain.pem', "utf-8")
+console.log("HSKEY")
 console.log(hskey)
+console.log("HSCERT")
 console.log(hscert)
+console.log("HSCHAIN")
 console.log(hschain)
 var serverOptions = {
     key: hskey,
