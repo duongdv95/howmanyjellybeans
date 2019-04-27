194c64eca51e99532a67afba4a3dfea48f6dad07
Daniel <danielvanduong@gmail.com>
2019-04-27 02:12:15 -0700

    HTTPS test

--- server/app.js
+++ server/app.js
@@ -33,6 +33,18 @@ if(env === "production") {
     app.use(cors())
     app.use(express.static(__dirname + '/../build/static', { dotfiles: 'allow' }))
     app.use(express.static(path.join(__dirname, '/../build')));
+    app.enable("trust proxy")
+    app.use(function(req, res, next) {
+        console.log("https:", req.secure)
+        if (req.secure) {
+            // request was via https, so do no special handling
+            return next();
+        } else {
+        console.log("http:", req.secure)
+        // request was via http, so redirect to https
+        res.redirect('https://' + req.headers.host + req.url);
+        }
+    })
     hskey     = fs.readFileSync('/etc/letsencrypt/live/howmanyjellybeans.com-0002/privkey.pem')
     hscert    = fs.readFileSync('/etc/letsencrypt/live/howmanyjellybeans.com-0002/cert.pem')
     hschain   = fs.readFileSync('/etc/letsencrypt/live/howmanyjellybeans.com-0002/chain.pem')
@@ -45,16 +57,6 @@ if(env === "production") {
         console.log('SSL Listening...')
     })
     io = socket.listen(server)
-    app.set("trust proxy")
-    app.use(function(req, res, next) {
-        if (req.secure) {
-        // request was via https, so do no special handling
-        return next();
-        } else {
-        // request was via http, so redirect to https
-        res.redirect('https://' + req.headers.host + req.url);
-        }
-    })
 }
 
 io.on('connection', function (socket) {
