d194c153112951b2038d2048e38eb056b3f67d9f
Daniel <danielvanduong@gmail.com>
2019-04-27 22:49:07 -0700

    Remove proxy file and added to app.js for development

--- src/setupProxy.js
+++
@@ -1,8 +0,0 @@
-const proxy = require('http-proxy-middleware');
-const env   = process.env.NODE_ENV || "development"
-
-if(env === "development") {
-    module.exports = function(app) {
-      app.use(proxy('/api', { target: 'http://localhost:5000/'}));
-    };
-}
